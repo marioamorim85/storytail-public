34320b24309ce56313cc51a5135fa4cc397169ca
Mário Amorim <mario.f.amorim@gmail.com>
2025-01-03 10:20:00 +0000

    commit

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        // Adicione quaisquer rotas que devam ser excluídas da verificação CSRF
+        //
     ];
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\VerifyCsrfToken;
+
 
 
 
