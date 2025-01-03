98ac543109543917f2d9ad397b14f9a22adcf9da
Mário Amorim <mario.f.amorim@gmail.com>
2025-01-03 15:55:06 +0000

    commit

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class VerifyCsrfToken extends Middleware
 {
@@ -15,23 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        // Adiciona aqui as rotas que devem ser excluídas da verificação CSRF
+        // Adicionar aqui as rotas que devem ser excluídas da verificação CSRF
     ];
-
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        try {
-            return parent::handle($request, $next);
-        } catch (\Illuminate\Session\TokenMismatchException $e) {
-            return redirect()->back()
-                ->withErrors(['CSRF' => 'Ocorreu um erro de segurança. Por favor, recarregue a página e tente novamente.']);
-        }
-    }
 }
