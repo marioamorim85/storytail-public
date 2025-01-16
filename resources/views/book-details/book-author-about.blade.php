4ac176e822fc73265e133d6af6625f9010ff6779
Mário Amorim <mario.f.amorim@gmail.com>
2025-01-16 09:44:33 +0000

    commit

--- resources/views/book-details/book-author-about.blade.php
+++ resources/views/book-details/book-author-about.blade.php
@@ -4,7 +4,7 @@
     <div class="fixed-header">
         <div class="st-book-banner">
             {{-- Título com fundo branco sobre o banner --}}
-            <h1 class="st-author-title">About he author</h1>
+            <h1 class="st-author-title">About the author</h1>
         </div>
     </div>
 
